b1e2ffa36cd03165e6d98ede40f5c2a586d42782
harishpython1 <harish.python.com@gmail.com>
2025-02-20 11:10:51 +0100

    Fixed server.js for Render

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
+
 dotenv.config(); // Load environment variables from .env
 
 const app = express();
